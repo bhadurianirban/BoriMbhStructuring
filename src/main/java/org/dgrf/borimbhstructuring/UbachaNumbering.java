197e5c394f07bf993d7a21a8ce00e5bf5c323470
bhadurianirban <bhaduri.anirban@gmail.com>
2019-04-17 21:35:22 +0530

    home

--- src/main/java/org/dgrf/borimbhstructuring/UbachaNumbering.java
+++ src/main/java/org/dgrf/borimbhstructuring/UbachaNumbering.java
@@ -28,71 +28,47 @@ import org.dgrf.borimbhstructuring.entities.Ubacha;
  */
 public class UbachaNumbering {
 
+    UbachaDAO ubachaDAO;
+    EntityManagerFactory emf;
+
     public void UbachaNumbering() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.dgrf_BoriMbhStructuring_jar_1.0-SNAPSHOTPU");
-        UbachaDAO ubachaDAO = new UbachaDAO(emf);
-        Ubacha ubacha;
+        emf = Persistence.createEntityManagerFactory("org.dgrf_BoriMbhStructuring_jar_1.0-SNAPSHOTPU");
+
         MainTextDAO mainTextDAO = new MainTextDAO(emf);
-        Maintext maintext = new Maintext();
-        Parva parva = new Parva();
-        MaintextPK maintextPK = new MaintextPK();
-        
-        
-        String csvFile = "/home/dgrfi/MEGA/MBHIndex/DevNagari/output/Mahabharat.csv";
-        
-        
+
+        String csvFile = "/home/bhaduri/MEGA/MBHIndex/DevNagari/output/Mahabharat.csv";
+
         BufferedReader br = null;
         String line = "";
-        String cvsSplitBy = ",";
-        
-        Date createDate = new Date();
-        
-        String parvaId;
-        String adhyayId;
-        int ubachaId;
-        String ubachaName;
-        String shlokaNum;
-        String shlokaLine;
-        String firstChar;
-        String shlokaText;
-        String endChar;
+
         int rowCount = 0;
         EntityManager em = mainTextDAO.getEntityManager();
+        em.getTransaction().begin();
         try {
 
             br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
-                String[] shlokaCSVLine = line.split(cvsSplitBy);
-                parvaId = shlokaCSVLine[0];
-                adhyayId = shlokaCSVLine[1];
-                ubachaName = shlokaCSVLine[2];
-                ubacha = ubachaDAO.getUbachaByName(ubachaName);
-                ubachaId = ubacha.getId();
-                shlokaNum = shlokaCSVLine[3];
-                shlokaLine = shlokaCSVLine[4];
-                shlokaText = shlokaCSVLine[5];
-                firstChar = shlokaText.substring(0, 1);
-                endChar = shlokaCSVLine[6];
-                maintextPK.setParvaId(Integer.parseInt(parvaId));
-                maintextPK.setAdhyayid(Integer.parseInt(adhyayId));
-                maintextPK.setShlokanum(Integer.parseInt(shlokaNum));
-                maintextPK.setShlokaline(Integer.parseInt(shlokaLine));
-                parva.setId(Integer.parseInt(parvaId));
-                maintext.setParva(parva);
-                maintext.setMaintextPK(maintextPK);
-                maintext.setEndchar(endChar);
-                maintext.setFirstchar(firstChar);
-                maintext.setShlokatext(shlokaText);
-                maintext.setUbachaId(ubacha);
-                maintext.setLastupdatedts(createDate);
                 try {
-                    mainTextDAO.create(maintext);
+                    Maintext maintext = createMainTextEntity(line);
+                    
+                    em.persist(maintext);
+
+                    if (rowCount % 1000 == 0) {
+                        System.out.println("Committing " + rowCount);
+                        em.flush();
+                        em.clear();
+
+                    }
+                    if (rowCount > 152340) {
+                        System.out.println(maintext.toString());
+                    }
                 } catch (Exception ex) {
                     Logger.getLogger(UbachaNumbering.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                System.out.println(parvaId+","+adhyayId+","+ubachaId+","+shlokaNum+","+shlokaLine+","+shlokaText+","+firstChar+","+endChar);
+                rowCount++;
+                //System.out.println(parvaId+","+adhyayId+","+ubachaId+","+shlokaNum+","+shlokaLine+","+shlokaText+","+firstChar+","+endChar);
             }
 
         } catch (FileNotFoundException e) {
@@ -108,8 +84,53 @@ public class UbachaNumbering {
                 }
             }
         }
+        em.flush();
+        em.clear();
+        em.getTransaction().commit();
 
     }
 
+    public Maintext createMainTextEntity(String line) {
+        String cvsSplitBy = ",";
+        ubachaDAO = new UbachaDAO(emf);
+        Date createDate = new Date();
+        Ubacha ubacha;
+        String parvaId;
+        String adhyayId;
+        int ubachaId;
+        String ubachaName;
+        String shlokaNum;
+        String shlokaLine;
+        String firstChar;
+        String shlokaText;
+        String endChar;
+        String[] shlokaCSVLine = line.split(cvsSplitBy);
+        parvaId = shlokaCSVLine[0];
+        adhyayId = shlokaCSVLine[1];
+        ubachaName = shlokaCSVLine[2];
+        ubacha = ubachaDAO.getUbachaByName(ubachaName);
+        ubachaId = ubacha.getId();
+        shlokaNum = shlokaCSVLine[3];
+        shlokaLine = shlokaCSVLine[4];
+        shlokaText = shlokaCSVLine[5];
+        firstChar = shlokaText.substring(0, 1);
+        endChar = shlokaCSVLine[6];
+        Maintext maintext = new Maintext();
+        Parva parva = new Parva();
+        MaintextPK maintextPK = new MaintextPK();
+        maintextPK.setParvaId(Integer.parseInt(parvaId));
+        maintextPK.setAdhyayid(Integer.parseInt(adhyayId));
+        maintextPK.setShlokanum(Integer.parseInt(shlokaNum));
+        maintextPK.setShlokaline(Integer.parseInt(shlokaLine));
+        parva.setId(Integer.parseInt(parvaId));
+        maintext.setParva(parva);
+        maintext.setMaintextPK(maintextPK);
+        maintext.setEndchar(endChar);
+        maintext.setFirstchar(firstChar);
+        maintext.setShlokatext(shlokaText);
+        maintext.setUbachaId(ubacha);
+        maintext.setLastupdatedts(createDate);
+        return maintext;
+    }
 
 }
